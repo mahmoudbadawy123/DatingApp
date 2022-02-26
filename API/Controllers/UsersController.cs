b349610a125e7e4b16a9173b242458e8dfcb0f1d
Mahmoud Badawy <mahmoudbadawy2020@gmail.com>
2022-02-26 22:18:05 +0200

    Add project files.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dbContext;
+
+        public UsersController( DataContext dbContext )
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("GetAllUsers")]
+        public async Task< ActionResult<List<AppUser>>> GetAllUsers()
+        {
+            var Users = await _dbContext.Users.ToListAsync();
+            return Users;
+        }
+
+        [HttpGet]
+        [Route("GetUsers/{Id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int Id)
+        {
+            var User = await _dbContext.Users.FirstOrDefaultAsync(_=>_.Id == Id);
+            return User;
+        }
+    }
+}
